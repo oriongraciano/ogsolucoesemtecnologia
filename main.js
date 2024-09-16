fc80c2995f1ecad87d61c776537d9252fbea7457
Orion Graciano <87495924+oriongraciano@users.noreply.github.com>
2024-09-15 23:06:03 -0300

    Add função botão cadastrar

--- main.js
+++ main.js
@@ -2,3 +2,13 @@
 function rolarParaTopo() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+function pgManutecao (){
+    alert("Pagina em Manutenção. Tente novamente mais tarde!")
+}
+
+let cadastrar = document.querySelector("#cadastrarCliente")
+
+cadastrar.addEventListener("click", () => {
+    pgManutecao()
+})
